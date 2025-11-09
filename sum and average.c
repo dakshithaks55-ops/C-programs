09bcf4b747f16b275825e7013c5100361954def1
dakshithaks55-ops <dakshithaks55@gmail.com>
2025-11-10 00:08:08 +0530

    sum and average.c
    Fix missing semicolon and incorrect printf format.

---
+++ sum and average.c
@@ -0,0 +1,12 @@
+1)#include <stdio.h>
+
+int main() {
+    int num1, num2, num3;
+    int sum, average;
+    printf("Enter any three integers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    sum = num1 + num2 + num3;
+    average = sum / 3
+    printf("Sum of the three numbers: %d %d %d\n", sum,average);
+     return 0; 
+}
